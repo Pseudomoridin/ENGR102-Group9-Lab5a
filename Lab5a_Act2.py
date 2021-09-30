e612d2c5d3dba277cab7c394a7dc9e345ff0d675
Bryce Elphingstone <73117281+Pseudomoridin@users.noreply.github.com>
2021-09-30 16:06:17 +0000

    please god work

--- Lab5a_Act2.py
+++ Lab5a_Act2.py
@@ -1,3 +1,14 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+# Names:        Elayne Elphingstone
+#               Reagan Wall
+#               Logan Winship
+#               Tyler Mayou & Zack Abbott
+# Section:      472/572
+# Assignment:   Lab 5A Activity 2
+# Date:         September 29, 2021
+
 from reference import reference as r
 
 ############################################
@@ -36,13 +47,13 @@ if gender == "M":
 else:
   reference = r[1]
 
-reference[1] = reference[1][agegroup]
-reference[2] = reference[2][agegroup]
+reference[0][1] = reference[0][1][agegroup]
+reference[0][2] = reference[0][2][agegroup]
 
 if medicated:
-  reference[4] = reference[4][0]
+  reference[0][4] = reference[0][4][0]
 else:
-  reference[4] = reference[4][1]
+  reference[0][4] = reference[0][4][1]
 
 ############################################
 ############ The Actual Program ############
@@ -50,66 +61,66 @@ else:
 score = 0
 # Age
 if age < 35:
-  score += reference[0][0]
+  score += reference[0][0][0]
 elif age < 40:
-  score += reference[0][1]
+  score += reference[0][0][1]
 elif age < 45:
-  score += reference[0][2]
+  score += reference[0][0][2]
 elif age < 50:
-  score += reference[0][3]
+  score += reference[0][0][3]
 elif age < 55:
-  score += reference[0][4]
+  score += reference[0][0][4]
 elif age < 60:
-  score += reference[0][5]
+  score += reference[0][0][5]
 elif age < 65:
-  score += reference[0][6]
+  score += reference[0][0][6]
 elif age < 70:
-  score += reference[0][7]
+  score += reference[0][0][7]
 elif age < 75:
-  score += reference[0][8]
+  score += reference[0][0][8]
 else:
-  score += reference[0][9]
+  score += reference[0][0][9]
 
 # Cholesterol
 if cholesterol < 160:
-  score += reference[1][0]
+  score += reference[0][1][0]
 elif cholesterol < 200:
-  score += reference[1][1]
+  score += reference[0][1][1]
 elif cholesterol < 240:
-  score += reference[1][2]
+  score += reference[0][1][2]
 elif cholesterol < 280:
-  score += reference[1][3]
+  score += reference[0][1][3]
 else:
-  score += reference[1][4]
+  score += reference[0][1][4]
 
 # Smoking
 if smoker:
-  score += reference[2][1]
+  score += reference[0][2][1]
 
 # HDL
 if hdl > 60:
-  score += reference[3][0]
+  score += reference[0][3][0]
 elif hdl > 50:
-  score += reference[3][1]
+  score += reference[0][3][1]
 elif hdl > 40:
-  score += reference[3][2]
+  score += reference[0][3][2]
 else: 
-  score += reference[3][3]
+  score += reference[0][3][3]
 
 # Blood Pressure
 if bp < 120:
-  score += reference[4][0]
+  score += reference[0][4][0]
 elif bp < 130:
-  score += reference[4][1]
+  score += reference[0][4][1]
 elif bp < 140:
-  score += reference[4][2]
+  score += reference[0][4][2]
 elif bp < 160:
-  score += reference[4][3]
+  score += reference[0][4][3]
 else:
-  score += reference[4][4]
+  score += reference[0][4][4]
 
 # Percent calculation
-for key in reference[5].keys():
+for key in reference[0][5].keys():
   if score < key:
-    print("Your 10-year risk of a heart attack is {}%".format(reference[5][key]))
+    print("Your 10-year risk of a heart attack is {}%".format(reference[0][5][key]))
     break
--- reference.py
+++ reference.py
@@ -1,3 +1,14 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+# Names:        Elayne Elphingstone
+#               Reagan Wall
+#               Logan Winship
+#               Tyler Mayou & Zack Abbott
+# Section:      472/572
+# Assignment:   Lab 5A Activity 2
+# Date:         September 29, 2021
+
 reference = [ # I'm gonna be honest this is probably the worst way to do this but I'm tired and it works so...
   [ # Male
     [
